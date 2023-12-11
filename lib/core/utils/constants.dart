abba5770b60df70163abc4c8dcfa8475f5b1b43a
Bosohyun <78394554+Bosohyun@users.noreply.github.com>
2023-12-11 07:43:10 -0500

    Update constants.dart

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,4 @@
 const kDefaultAvatar = 'https://images.freeimages'
     '.com/fic/images/icons/573/must_have/256/user.png';
 
-const openAIAPI = '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';
+const openAIAPI = ''
